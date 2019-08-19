ef656ae02bfe70faea1d3b99336f1f1eee121ee4
Dillon <49077521+dillonjohnbrown@users.noreply.github.com>
2019-08-19 14:18:13 -0700

    Structure update

--- _build/js/custom.js
+++ _build/js/custom.js
@@ -1,6 +1,9 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {
+  hideWordMark()
+};
 
-function myFunction() {
+//
+function hideWordMark() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     $("#navLogo").addClass("scrolled");
   } else {
