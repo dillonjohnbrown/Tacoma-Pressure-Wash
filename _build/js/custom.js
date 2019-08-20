246bc233a7258efff50797d396f30e4ff93ff019
Dillon <49077521+dillonjohnbrown@users.noreply.github.com>
2019-08-19 17:40:19 -0700

    Add nav, add CSS

--- _build/js/custom.js
+++ _build/js/custom.js
@@ -2,11 +2,13 @@ window.onscroll = function() {
   hideWordMark()
 };
 
-//
+// Hide word part of logo on scroll
 function hideWordMark() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     $("#navLogo").addClass("scrolled");
+    $("#navLink1, #navLink2, #navLink3").addClass("scrolled");
   } else {
     $("#navLogo").removeClass("scrolled");
+    $("#navLink1, #navLink2, #navLink3").removeClass("scrolled");
   }
 }
