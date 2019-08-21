29b21c761cc7bf85363bae4b059eff7d3b385d9e
Dillon <49077521+dillonjohnbrown@users.noreply.github.com>
2019-08-21 13:36:40 -0700

    Add map and animation for locations

--- _build/js/custom.js
+++ _build/js/custom.js
@@ -1,5 +1,21 @@
+var cities = [
+  $("#Tacoma"),
+  $("#Gig-Harbor"),
+  $("#Steilacoom"),
+  $("#Fircrest"),
+  $("#Lakewood"),
+  $("#Puyallup"),
+  $("#Fife"),
+  $("#Federal-Way"),
+  $("#University-Place"),
+  $("#Milton"),
+  $("#Ruston")
+];
+var animationTime = 1500;
+
 window.onscroll = function() {
-  hideWordMark()
+  hideWordMark();
+  map();
 };
 
 // Hide word part of logo on scroll
@@ -12,4 +28,24 @@ function hideWordMark() {
     $("#navLink1, #navLink2, #navLink3").removeClass("fadeInUp");
     $("#wordmark").removeClass("fadeOutUp");
   }
+}
+
+function map() {
+  var element           = $("#more");
+  var top_of_element    = element.offset().top;
+  var bottom_of_element = top_of_element + element.outerHeight();
+  var top_of_screen     = $(window).scrollTop();
+  var bottom_of_screen  = top_of_screen + $(window).innerHeight();
+
+
+    if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+        // Show each city graphic
+        $("#WSDOT__City_Limits path").each(function(index) {
+          $(this).delay(animationTime * index).fadeIn(animationTime);
+        });
+        // Show each city text
+        $("#city-list li").each(function(index) {
+          $(this).delay(animationTime * index).fadeIn(animationTime);
+        });
+    }
 }
