6ac2e9ddf54c64a3939936d472f46e87b4e03b37
Dillon <49077521+dillonjohnbrown@users.noreply.github.com>
2019-08-20 21:19:24 -0700

    Improve style, page layout, and animation

--- _build/js/custom.js
+++ _build/js/custom.js
@@ -5,10 +5,10 @@ window.onscroll = function() {
 // Hide word part of logo on scroll
 function hideWordMark() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    $("#navLogo").addClass("scrolled");
     $("#navLink1, #navLink2, #navLink3").addClass("scrolled");
+    $("#wordmark").addClass("fadeOutUp");
   } else {
-    $("#navLogo").removeClass("scrolled");
     $("#navLink1, #navLink2, #navLink3").removeClass("scrolled");
+    $("#wordmark").removeClass("fadeOutUp");
   }
 }
