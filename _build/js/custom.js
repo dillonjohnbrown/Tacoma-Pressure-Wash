6d309400af3b6249ce2cf35bac88c0122a1d9b8d
Dillon <49077521+dillonjohnbrown@users.noreply.github.com>
2019-08-22 12:33:56 -0700

    Change fade behavior

--- _build/js/custom.js
+++ _build/js/custom.js
@@ -49,11 +49,11 @@ function map() {
     if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
         // Show each city graphic
         $("#WSDOT__City_Limits path").each(function(index) {
-          $(this).delay(animationTime * index).fadeIn(animationTime);
+          $(this).delay(animationTime * index).fadeTo(animationTime, 0.1);
         });
         // Show each city text
         $("#city-list li").each(function(index) {
-          $(this).delay(animationTime * index).fadeIn(animationTime);
+          $(this).delay(animationTime * index).fadeTo(animationTime, 1);
         });
     }
 }
